82466bfc145aa701a564fb7bfb48de0c32ef6d39
roberto.pesce <robertopescee@hotmail.com>
2020-06-13 11:18:29 +0200

    lp, auth and initial models

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, request, url_for
 from flask_login import login_required, current_user
 import datetime
+import flask
 
+# from models import InstagramAccount
+from app import db
 
 year = datetime.datetime.now().strftime("%Y")
 main = Blueprint('main', __name__)
@@ -17,3 +20,26 @@ def index(y=year):
 def profile(y=year):
     return render_template('dashboard.html', year=y, name=current_user.name)
 
+
+@main.route('/add-instagram', methods=['POST'])
+@login_required
+def add_instagram():
+    return render_template('home.html', year=2011)
+    # return render_template('home.html', year=2020)
+    #if flask.request.method == 'POST':
+    #    instagram_username = request.form.get('instagram_username')
+    #    instagram_password = request.form.get('instagram_password')
+
+    #    instagram_account = InstagramAccount.query.filter_by(instagram_username=instagram_username).first()
+
+    #    if instagram_account:
+    #        flash('This account has already been configured')
+    #        return redirect(url_for('main.dashboard'))
+
+    #    new_instagram_account = InstagramAccount(instagram_username=instagram_username, instagram_password=instagram_passwor)
+    #    db.session.add(new_instagram_account)
+    #    db.session.commit()
+
+    #    return redirect(url_for('main.dashboard'))
+    #else:
+    #    return flash('This operation is not authorized')
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from flask_login import UserMixin
 from app import db
+# from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(UserMixin, db.Model):
@@ -7,3 +9,17 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    #insta_account = relationship("InstagramAccount", back_populates="user")
+
+
+#class InstagramAccount(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    user_id = db.Column(Integer, ForeignKey('user.id'))
+#    instagram_username = db.Column(db.String(500))
+#    instagram_password = db.Column(db.String(500))
+
+
+#class InstagramConfigs(db.Model):
+#    pass
+
+
