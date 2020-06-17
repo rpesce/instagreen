85f02e35c2030d8215fb2fdc0defc0b3acba7f7a
roberto.pesce <robertopescee@hotmail.com>
2020-06-17 23:35:14 +0200

    Added instagram accounts to dashboard views and templates

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import flask_login
 from flask import Blueprint, render_template, flash, redirect, request, url_for
 from flask_login import login_required, current_user
 import datetime
 import flask
 
-# from models import InstagramAccount
 from app import db
+from models import InstagramAccount
 
 year = datetime.datetime.now().strftime("%Y")
 main = Blueprint('main', __name__)
@@ -15,31 +16,32 @@ def index(y=year):
     return render_template('home.html', year=y)
 
 
-@main.route('/dashboard')
+@main.route('/dashboard', methods=['GET'])
 @login_required
 def profile(y=year):
-    return render_template('dashboard.html', year=y, name=current_user.name)
+    user_id = current_user.id
+    instagram_accounts = InstagramAccount.query.filter_by(user_id=user_id)
+    return render_template('dashboard.html', year=y, name=current_user.name, instagram_accounts=instagram_accounts)
 
 
 @main.route('/add-instagram', methods=['POST'])
 @login_required
 def add_instagram():
-    return render_template('home.html', year=2011)
-    # return render_template('home.html', year=2020)
-    #if flask.request.method == 'POST':
-    #    instagram_username = request.form.get('instagram_username')
-    #    instagram_password = request.form.get('instagram_password')
+    if flask.request.method == 'POST':
+        instagram_username = request.form.get('instagram_username')
+        instagram_password = request.form.get('instagram_password')
 
-    #    instagram_account = InstagramAccount.query.filter_by(instagram_username=instagram_username).first()
+        instagram_account = InstagramAccount.query.filter_by(instagram_username=instagram_username).first()
 
-    #    if instagram_account:
-    #        flash('This account has already been configured')
-    #        return redirect(url_for('main.dashboard'))
+        if instagram_account:
+            flash('This Instagram account has already been enabled')
+            return redirect(url_for('main.profile'))
 
-    #    new_instagram_account = InstagramAccount(instagram_username=instagram_username, instagram_password=instagram_passwor)
-    #    db.session.add(new_instagram_account)
-    #    db.session.commit()
+        new_instagram_account = InstagramAccount(instagram_username=instagram_username, instagram_password=instagram_password, user_id=current_user.id)
+        db.session.add(new_instagram_account)
+        db.session.commit()
 
-    #    return redirect(url_for('main.dashboard'))
-    #else:
-    #    return flash('This operation is not authorized')
+        flash('Instagram account successfully added')
+        return redirect(url_for('main.profile'))
+    else:
+        return flash('This operation is not authorized')
--- models.py
+++ models.py
@@ -43,33 +43,40 @@ class InstagramConfigs(db.Model):
     like_by_tags_and_interact_percentage = db.Column(db.Integer, nullable=False, default=100) # I added default
     like_by_tags_and_interact_media = db.Column(db.String(50), nullable=False, default=None) # I added default
 
+    like_by_feed_enabled = db.Column(db.Boolean, default=False, nullable=False)
     like_by_feed_amount = db.Column(db.Integer, nullable=False, default=10) # I added default
     like_by_feed_randomize = db.Column(db.Boolean, nullable=False, default=True) # I added default
     like_by_feed_unfollow = db.Column(db.Boolean, nullable=False, default=True) # I added default
     like_by_feed_interact = db.Column(db.Boolean, nullable=False, default=True) # I added default
 
+    like_by_locations_enabled = db.Column(db.Boolean, default=False, nullable=False)
     like_by_locations_locations = db.Column(db.String(1000), nullable=False, default=None)#My default
     like_by_locations_amount = db.Column(db.Integer, nullable=False, default=100)#My default
     like_by_locations_skip_to_posts = db.Column(db.Boolean, nullable=False, default=False)#My default
 
+    follow_by_tags_enabled = db.Column(db.Boolean, default=False, nullable=False)
     follow_by_tags_tags = db.Column(db.String(1000), nullable=False, default=None) # I added default
     follow_by_tags_amount = db.Column(db.Integer, nullable=False, default=10) # I added default
     follow_by_tags_skip_top_posts = db.Column(db.Boolean, nullable=False, default=True) # I added default
     follow_by_tags_randomize = db.Column(db.Boolean, nullable=False, default=False) # I added default
     follow_by_tags_media = db.Column(db.String(50), nullable=False, default=None) # I added default
 
+    follow_by_locations_enabled = db.Column(db.Boolean, default=False, nullable=False)
     follow_by_locations_locations = db.Column(db.String(1000), nullable=False, default=None)#My default
     follow_by_locations_amount = db.Column(db.Integer, nullable=False, default=100)#My default
     follow_by_locations_skip_to_posts = db.Column(db.Boolean, nullable=False, default=False)#My default
 
+    follow_user_followers_enabled = db.Column(db.Boolean, default=False, nullable=False)
     follow_user_followers_users = db.Column(db.String(1000), nullable=False, default=None)
     follow_user_followers_amount = db.Column(db.Integer, nullable=False, default=10)#My default
     follow_user_followers_randomize = db.Column(db.Boolean, nullable=False, default=False)#My default
 
+    follow_user_following_enabled = db.Column(db.Boolean, default=False, nullable=False)
     follow_user_following_users = db.Column(db.String(1000), nullable=False, default=None)
     follow_user_following_amount = db.Column(db.Integer, nullable=False, default=10)#My default
     follow_user_following_randomize = db.Column(db.Boolean, nullable=False, default=False)#My default
 
+    follow_and_interact_user_followers_enabled = db.Column(db.Boolean, default=False, nullable=False)
     follow_and_interact_user_followers_followers = db.Column(db.String(1000), nullable=False, default=None)
     follow_and_interact_user_followers_amount_interact = db.Column(db.Integer, nullable=False, default=5)
     follow_and_interact_user_followers_randomize_interact =db.Column(db.Boolean, nullable=False, default=True)
@@ -79,6 +86,7 @@ class InstagramConfigs(db.Model):
     follow_and_interact_user_followers_randomize_followers =db.Column(db.Boolean, nullable=False, default=False)
     follow_and_interact_user_followers_interact = db.Column(db.Boolean, nullable=False, default=True)
 
+    follow_likers_enabled = db.Column(db.Boolean, default=False, nullable=False)
     follow_likers_users = db.Column(db.String(1000), nullable=False, default=None)
     follow_likers_amount = db.Column(db.Integer, nullable=False, default=2)
     follow_likers_percentage = db.Column(db.Integer, nullable=False, default=70)
@@ -90,6 +98,7 @@ class InstagramConfigs(db.Model):
     follow_likers_sleep_delay = db.Column(db.Integer, nullable=False, default=600)
     follow_likers_interact = db.Column(db.Boolean, nullable=False, default=False)
 
+    unfollow_instapy_users_enabled = db.Column(db.Boolean, default=False, nullable=False)
     unfollow_instapy_users_amount = db.Column(db.Integer, nullable=False, default=60)
     unfollow_instapy_users_instapy_followed_enabled = db.Column(db.Boolean, nullable=False, default=True)
     unfollow_instapy_users_instapy_followed_param = db.Column(db.String(1000), nullable=False, default="nonfollowers")
@@ -97,23 +106,30 @@ class InstagramConfigs(db.Model):
     unfollow_instapy_users_unfollow_after = db.Column(db.Integer, nullable=False, default=324000)
     unfollow_instapy_users_sleep_delay = db.Column(db.Integer, nullable=False, default=501)
 
+    unfollow_users_who_dont_follow_back_enabled = db.Column(db.Boolean, default=False, nullable=False)
     unfollow_users_who_dont_follow_back_amount = db.Column(db.Integer, nullable=False, default=126)
     unfollow_users_who_dont_follow_back_nonFollowers = db.Column(db.Boolean, nullable=False, default=True)
     unfollow_users_who_dont_follow_back_style = db.Column(db.String(1000), nullable=False, default="RANDOM")
     unfollow_users_who_dont_follow_back_unfollow_after = db.Column(db.Integer, nullable=False, default=151200)
     unfollow_users_who_dont_follow_back_sleep_delay = db.Column(db.Integer, nullable=False, default=655)
 
-    accept_follow_requests_amount = db.Column(db.Integer, nullable=False, default=100)
+    accept_follow_requests_enabled = db.Column(db.Boolean, default=False, nullable=False)
     accept_follow_requests_sleep_delay = db.Column(db.Integer, nullable=False, default=1)
+    accept_follow_requests_amount = db.Column(db.Integer, nullable=False, default=100)
 
+    remove_follow_requests_enabled = db.Column(db.Boolean, default=False, nullable=False)
     remove_follow_requests_amount = db.Column(db.Integer, nullable=False, default=200)
     remove_follow_requests_sleep_delay = db.Column(db.Integer, nullable=False, default=600)
 
+    skip_users_enabled = db.Column(db.Boolean, default=False, nullable=False)
     skip_users_keywords = db.Column(db.String(2000), nullable=False, default=None)
 
+    restricting_likes_keywords_enabled = db.Column(db.Boolean, default=False, nullable=False)
     restricting_likes_keywords = db.Column(db.String(2000), nullable=False, default=None)
 
+    ignoring_users_friends_enabled = db.Column(db.Boolean, default=False, nullable=False)
     ignoring_users_friends = db.Column(db.String(2000), nullable=False, default=None)
 
+    excluding_friends_friends_enabled = db.Column(db.Boolean, default=False, nullable=False)
     excluding_friends_friends = db.Column(db.String(2000), nullable=False, default=None)
 
